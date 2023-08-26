699c4b72b981da8fe4d88aacc55c69cac2054482
VadimKurachynski <kurachynski@gmail.com>
2023-08-26 10:51:39 +0300

    45

--- js/index.js
+++ js/index.js
@@ -53,7 +53,7 @@ function prov() {
 function provglavnaja() {
   var el = document.getElementById('kod').value;
   el = el.toLowerCase();
-  if (el == "111") {
+  if (el == "47164029") {
     document.getElementById('dkod').style.display = 'initial';
     document.getElementById('messagekod').value = "ВЕРНО!"
   } else {
